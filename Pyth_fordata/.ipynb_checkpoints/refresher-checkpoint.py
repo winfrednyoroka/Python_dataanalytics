7f4cbb93b5cf8cd135fc517849cdef3adece11f0
winfrednyoroka <winfredgatua13@gmail.com>
2020-12-07 15:56:21 +0300

    added new files on python and data analytics

---
+++ Pyth_fordata/.ipynb_checkpoints/refresher-checkpoint.py
@@ -0,0 +1,37 @@
+
+print(5+7)
+print(8-10)
+print(9*11)
+print(-15/3)
+print(3**3)
+print(2**1/2)
+
+print((3.2*10**12)*2.5)
+
+## 1 gram is equal to 10^12 picograms
+print((2.3/10**12)*45.3)
+
+#remainder of dividing a number
+print(9%2)
+print(1522756%37) # it is not exactly divisible since there is a remainder
+#floor division
+print(22//7)
+
+print((5*1.20)+(8*2.15))
+
+print(1000/(20+30))
+print(((3*15.20)+(9*17.45)+(2*24))-(20/100*((3*15.20)+(9*17.45)+(2*24))))
+
+##strings
+print("Once upon a time")
+
+print('''Ready…
+Steady…
+Go!
+''')
+
+##print("Ready…
+\nSteady…
+\nGo!
+")
+print("Con"+"cat"+"en"+"ate")
